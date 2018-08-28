1f193bab79419da5920bef407c3d9d72d8bdc0d8
tinaning-pipi <tinaning-007pipi@163.com>
2018-08-28 17:55:01 +0800

    8-28

--- pages/lookfor/lookfor.js
+++ pages/lookfor/lookfor.js
@@ -5,7 +5,12 @@ Page({
    */
   data: {
     toView: 'red',
-    listA: [{ id: '1', name: '丢了只猫', time: '2018-08-15 10:38:09' }, { id: '2', name: '丢了钱包', time: '2018-08-23 10:38:09' }, { id: '3', name: '李莉的省市', time: '2018-08-23 14:58:09' }, { id: '4', name: '乐积分女', time: '2018-04-15 10:38:09' }, { id: '5', name: '胜多负少', time: '2018-04-15 10:38:09' }, { id: '6', name: '色色冯绍峰大幅度发斯蒂芬斯蒂芬', time: '2018-04-15 10:38:09' }, { id: '7', name: '省份省份水电费水电费水电费的发送到发送到发送到冯绍峰第三方士大夫士大夫', time: '2018-04-15 10:38:09' }]
+    srollHeight:0,
+    //记录当前点击功能id
+    dateCurrent:'',
+    //功能数据
+    listFun: [{ id: '1', name: '寻物启事' }, { id: '2', name: '失物招领' }, { id: '3', name: '寻人启事' }, { id: '4', name: '感谢信' }],
+    listA: [{ id: '1', img: '11.png', zan: 99, message: 30, name: '我在人民广场吃着炸鸡，结果把猫丢了，黑得发亮的猫猫', nic: 'lililili', time: '2018-08-28 15:28:09' }, { id: '2', img: '12.png', zan: 99, message: 30, nic: 'tinaning-007pipi', name: '丢了钱包', time: '2018-08-28 10:38:09' }, { id: '3', zan: 99, message: 30, img: '21.png', name: '李莉的省市', nic: 'linda-liu', zan: 99, message: 30, time: '2018-08-28 14:58:09' }, { id: '4', nic: '从前有只猫', img: 'cat1.jpg', zan: 99, message: 30, name: '乐积分女', time: '2018-08-27 10:38:09' }, { id: '5', img: 'cat2.jpg', zan: 99, message: 30, nic: '我去年买了个表', name: '胜多负少', time: '2018-04-15 10:38:09' }, { id: '6', zan: 99, message: 30, img: 'banana.jpg', name: '色色冯绍峰大幅度发斯蒂芬斯蒂芬', nic: '充了一罐奶粉', time: '2018-04-15 10:38:09' }, { id: '7', zan: 99, message: 30,img: '41.png', name: '省份省份水电费水电费水电费的发送到发送到发送到冯绍峰第三方士大夫士大夫', nic: '电脑好重呀', time: '2018-04-15 10:38:09' }]
   },
   /**
    * 生命周期函数--监听页面加载
@@ -13,12 +18,17 @@ Page({
   onLoad: function (options) {
 
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setData({ dateCurrent: this.data.listFun[0].id});
+    var app=this;
+    wx.getSystemInfo({
+      success: function (res) {
+        app.setData({ srollHeight: res.windowHeight - 30 });
+      },
+    })
   },
 
   /**
@@ -49,12 +59,23 @@ Page({
 
   },
   upper: function (e) {
-    console.log(e)
+    //console.log(e)
   },
   lower: function (e) {
-    console.log(e)
+    //console.log(e)
   },
   scroll: function (e) {
-    console.log(e)
+   // console.log(e)
+  },
+  /*功能切换事件，更改样式 */
+  chg:function(e){
+    var obj=e.target.id;
+    this.setData({ dateCurrent:obj});
+  },
+  showProd:function(e){
+    var obj = e.currentTarget.id;
+    wx.navigateTo({
+      url: '../product/product?id='+obj
+    })
   }
 })
--- pages/product/product.js
+++ pages/product/product.js
@@ -5,23 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    nowId:'',
+    ifZan: false,
+    ifLow: false,
+    markers:[],
+    proInfo: { id: '7', img: 'banana.jpg',zan:23453, name: '省份省份水电费水电费水电费的发送到发送到发送到冯绍峰第三方士大夫士大夫', nic: '电脑好重呀', time: '2018-04-15 10:38:09', remark: '本人于4月15日在四川省成都市高新区天府五街地铁口遗失小黄人一枚，请拾到者速与本人联系，此物危害性极大，容易把人萌翻，坠入动漫世界无法自拔。', lng: 104.076479, lat: 30.542615 }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({nowId:options.id})
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.setData({markers:[{
+      iconPath: "/img/narrow.png",
+      id: 0,
+      latitude: this.data.proInfo.lat,
+      longitude: this.data.proInfo.lng,
+      width: 25,
+      height: 25
+    }]})
   },
-
+  ckZan:function(){
+    if(!this.data.ifZan){
+      this.setData({ ifZan: true, ifLow: false,'proInfo.zan': this.data.proInfo.zan + 1 })
+    }    
+  },
+  ckLow: function () {
+    if (!this.data.ifLow) {
+      this.setData({ ifLow: true,ifZan:false, 'proInfo.zan': this.data.proInfo.zan - 1 })
+    }
+  },
   /**
    * 生命周期函数--监听页面显示
    */
